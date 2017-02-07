12cfb2db675d32df8e9c85bc0b4c85c18d0a3199
alexa <alexa@XEON>
2017-02-07 13:40:55 +0200

    Предварительный рабочий вариант

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -30,15 +30,7 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.noneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.eraseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -54,7 +46,6 @@
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.editToolStripMenuItem,
             this.toolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
@@ -65,73 +56,17 @@
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.clearToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
-            // clearToolStripMenuItem
-            // 
-            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
-            this.clearToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
-            this.clearToolStripMenuItem.Text = "Clear";
-            // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.exitToolStripMenuItem.Text = "Exit";
-            // 
-            // editToolStripMenuItem
-            // 
-            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.colorToolStripMenuItem,
-            this.eraseToolStripMenuItem});
-            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
-            this.editToolStripMenuItem.Text = "Edit";
-            // 
-            // colorToolStripMenuItem
-            // 
-            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.redToolStripMenuItem,
-            this.greenToolStripMenuItem,
-            this.blueToolStripMenuItem,
-            this.noneToolStripMenuItem});
-            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
-            this.colorToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
-            this.colorToolStripMenuItem.Text = "Color";
-            // 
-            // redToolStripMenuItem
-            // 
-            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
-            this.redToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
-            this.redToolStripMenuItem.Text = "Red";
-            // 
-            // greenToolStripMenuItem
-            // 
-            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
-            this.greenToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
-            this.greenToolStripMenuItem.Text = "Green";
-            // 
-            // blueToolStripMenuItem
-            // 
-            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
-            this.blueToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
-            this.blueToolStripMenuItem.Text = "Blue";
-            // 
-            // noneToolStripMenuItem
-            // 
-            this.noneToolStripMenuItem.Name = "noneToolStripMenuItem";
-            this.noneToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
-            this.noneToolStripMenuItem.Text = "None";
-            // 
-            // eraseToolStripMenuItem
-            // 
-            this.eraseToolStripMenuItem.Name = "eraseToolStripMenuItem";
-            this.eraseToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
-            this.eraseToolStripMenuItem.Text = "Erase";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // toolStripMenuItem1
             // 
@@ -188,9 +123,8 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(12, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Ready";
             // 
             // label2
             // 
@@ -217,6 +151,7 @@
             this.MinimizeBox = false;
             this.Name = "frmPaint";
             this.Text = "Paint";
+            this.Load += new System.EventHandler(this.FormLoad);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -228,20 +163,12 @@
         #endregion
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem noneToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem eraseToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.Button btnDraw;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
     }
--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -17,6 +17,7 @@ namespace Paint
         public frmPaint()
         {
             InitializeComponent();
+            
         }
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
@@ -26,83 +27,96 @@ namespace Paint
 
         private void helpToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            string str = "Что бы выбрать цвет пера надо: ......"; // Строковая переменная с описание помощи
+            string str = "Клавиши управления: \n 1 - Красный цвет \n 2 - Зеленый цвет \n 3- Синий цыет \n 0 - Перемещение курсора \n Е - Режим стирания \n Esc - Выход из режима рисования"; 
+
             MessageBox.Show(str, "Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void btnDraw_Click(object sender, EventArgs e)
         {
-            label1.Text = "Moving";
+            int dX, dY, sizeX, sizeY;
+            
             pictureBox1.Cursor = Cursors.Cross;
             btnDraw.Enabled = false;
             pictureBox1.Focus();
-            Cursor.Clip= new Rectangle(pictureBox1.Location, pictureBox1.Size);
-        }
+            dX = Cursor.Position.X - pictureBox1.PointToClient(Control.MousePosition).X;
+            dY = Cursor.Position.Y - pictureBox1.PointToClient(Control.MousePosition).Y;
+            Point PBStart = new Point();
+            PBStart.X = dX;
+            PBStart.Y = dY;
+            sizeX = pictureBox1.Size.Width - 2;
+            sizeY= pictureBox1.Size.Height - 2;
+            Cursor.Clip= new Rectangle(dX, dY, sizeX, sizeY);
 
-        private void button1_Click(object sender, EventArgs e)
-        {
             
-        }
+            
+    }
 
+        
         private void KeyPressed(object sender, PreviewKeyDownEventArgs e)
         {
+            Graphics g = pictureBox1.CreateGraphics();
             switch (e.KeyValue)
             {
                 case 37: //left
                     Cursor.Position = new Point ( Cursor.Position.X -1, Cursor.Position.Y);
                     
+                    g.DrawLine(new Pen(CursorDraw.CursorColor, 1), new Point(pictureBox1.PointToClient(Control.MousePosition).X+1, pictureBox1.PointToClient(Control.MousePosition).Y), new Point(pictureBox1.PointToClient(Control.MousePosition).X, pictureBox1.PointToClient(Control.MousePosition).Y));
                     break;
                 case 38: //up
                     Cursor.Position = new Point(Cursor.Position.X , Cursor.Position.Y-1);
+                    g.DrawLine(new Pen(CursorDraw.CursorColor, 1), new Point(pictureBox1.PointToClient(Control.MousePosition).X, pictureBox1.PointToClient(Control.MousePosition).Y+1), new Point(pictureBox1.PointToClient(Control.MousePosition).X, pictureBox1.PointToClient(Control.MousePosition).Y));
                     break;
                 case 39: //right
                     Cursor.Position = new Point(Cursor.Position.X + 1, Cursor.Position.Y);
+                    g.DrawLine(new Pen(CursorDraw.CursorColor, 1), new Point(pictureBox1.PointToClient(Control.MousePosition).X-1, pictureBox1.PointToClient(Control.MousePosition).Y), new Point(pictureBox1.PointToClient(Control.MousePosition).X, pictureBox1.PointToClient(Control.MousePosition).Y));
                     break;
                 case 40: //down
                     Cursor.Position = new Point(Cursor.Position.X , Cursor.Position.Y +1 );
+                    g.DrawLine(new Pen(CursorDraw.CursorColor, 1), new Point(pictureBox1.PointToClient(Control.MousePosition).X, pictureBox1.PointToClient(Control.MousePosition).Y), new Point(pictureBox1.PointToClient(Control.MousePosition).X, pictureBox1.PointToClient(Control.MousePosition).Y -1));
                     break; 
                 case 49:  //1
                     CursorDraw.CursorColor = Color.Red;
                     CursorDraw.Draw = true;
-                    label1.Text = "Drawing";
+                    label1.Text = "Красный цвет пера";
                     label1.ForeColor = Color.Red;
                     break;
                 case 50:  //2
                     CursorDraw.CursorColor = Color.Green;
                     CursorDraw.Draw = true;
-                    label1.Text = "Drawing";
+                    label1.Text = "Зеленый цвет пера";
                     label1.ForeColor = Color.Green;
                     break;
                 case 51:  //3
                     CursorDraw.CursorColor = Color.Blue;
                     CursorDraw.Draw = true;
-                    label1.Text = "Drawing";
+                    label1.Text = "Синий  цвет пера";
                     label1.ForeColor = Color.Blue;
                     break;
                 case 48:  //0
                     CursorDraw.CursorColor = Color.Transparent;
                     CursorDraw.Draw = false;
-                    label1.Text = "Moving";
+                    label1.Text = "Перемещение";
                     label1.ForeColor = Color.Black;
                     
                     break;
                 case 69:  //E
                     CursorDraw.CursorColor = Color.White;
                     CursorDraw.Draw = true;
-                    label1.Text = "Erasing";
+                    label1.Text = "Стирание";
                     label1.ForeColor = Color.Black;
 
                     break;
                 case 27: //Esc
                     pictureBox1.Cursor = Cursors.Default;
-                    label1.Text = "Waiting";
-                    label1.ForeColor = Color.Black;
+                    CursorDraw.Draw = false;
                     btnDraw.Enabled = true;
                     Cursor.Clip = new Rectangle();
                     break; 
             }
+            g.Dispose();
             
-            //MessageBox.Show("Pressed" + e.KeyValue, "Key pressed");
+            
                 
         }
 
@@ -118,7 +132,28 @@ namespace Paint
 
         private void pictureBox1_Click_1(object sender, EventArgs e)
         {
-            MessageBox.Show("Form: " + frmPaint.ActiveForm.Location.X +", " + frmPaint.ActiveForm.Location.Y + ". PictureBox: " + pictureBox1.Left + ", "+ pictureBox1.Top + ". " );
+            
+        }
+
+        private void FormLoad(object sender, EventArgs e)
+        {
+
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
